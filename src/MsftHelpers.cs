6486e2b5d25137decefb1c7e042924b2df3a00ff
N3ROO <swellchaser33@gmail.com>
2020-11-04 19:18:39 +0100

    removed useless class from MSFT

--- src/MsftHelpers.cs
+++ src/MsftHelpers.cs
@@ -80,61 +80,6 @@ namespace MSFTHelpers
     }
 
     /// <summary>
-    ///     Display class used to represent a BluetoothLEDevice in the Device list
-    /// </summary>
-    public class BluetoothLEDeviceDisplay : INotifyPropertyChanged
-    {
-        public BluetoothLEDeviceDisplay(DeviceInformation deviceInfoIn)
-        {
-            DeviceInformation = deviceInfoIn;
-            UpdateGlyphBitmapImage();
-        }
-
-        public DeviceInformation DeviceInformation { get; private set; }
-
-        public string Id => DeviceInformation.Id;
-        public string Name => DeviceInformation.Name;
-        public bool IsPaired => DeviceInformation.Pairing.IsPaired;
-        public bool IsConnected => (bool?)DeviceInformation.Properties["System.Devices.Aep.IsConnected"] == true;
-        public bool IsConnectable => (bool?)DeviceInformation.Properties["System.Devices.Aep.Bluetooth.Le.IsConnectable"] == true;
-
-        public IReadOnlyDictionary<string, object> Properties => DeviceInformation.Properties;
-
-        public BitmapImage GlyphBitmapImage { get; private set; }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public void Update(DeviceInformationUpdate deviceInfoUpdate)
-        {
-            DeviceInformation.Update(deviceInfoUpdate);
-
-            OnPropertyChanged("Id");
-            OnPropertyChanged("Name");
-            OnPropertyChanged("DeviceInformation");
-            OnPropertyChanged("IsPaired");
-            OnPropertyChanged("IsConnected");
-            OnPropertyChanged("Properties");
-            OnPropertyChanged("IsConnectable");
-
-            UpdateGlyphBitmapImage();
-        }
-
-        private async void UpdateGlyphBitmapImage()
-        {
-            DeviceThumbnail deviceThumbnail = await DeviceInformation.GetGlyphThumbnailAsync();
-            var glyphBitmapImage = new BitmapImage();
-            await glyphBitmapImage.SetSourceAsync(deviceThumbnail);
-            GlyphBitmapImage = glyphBitmapImage;
-            OnPropertyChanged("GlyphBitmapImage");
-        }
-
-        protected void OnPropertyChanged(string name)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-    }
-
-    /// <summary>
     ///     This enum assists in finding a string representation of a BT SIG assigned value for Service UUIDS
     ///     Reference: https://developer.bluetooth.org/gatt/services/Pages/ServicesHome.aspx
     /// </summary>
